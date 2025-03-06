39116627fcde9da06b9e529b39374a106668632a
Rafael Vieira <mouravieira44@gmail.com>
2025-03-05 23:08:48 -0300

    add EmptyTestCheck sem revisao

---
+++ src/main/java/br/ufba/testsmells/checks/EmptyTestCheck.java
@@ -0,0 +1,86 @@
+package br.ufba.testsmells.checks;
+
+import com.puppycrawl.tools.checkstyle.StatelessCheck;
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+@StatelessCheck
+public class EmptyTestCheck extends AbstractCheck {
+
+    // TODO: TESTAR CLASSE EmptyTestCheck
+    // Exemplos para teste:
+//    @Test
+//    public void testEmpty() {}
+//
+//    @Test
+//    public void testWithComments() {
+//        // Este é um comentário
+//    }
+//
+//    @Test
+//    public void testValid() {
+//        assertEquals(2, 1 + 1);
+//    }
+//
+//    @Test
+//    public void testWithSemi() {
+//        ;
+//    }
+
+    @Override
+    public int[] getAcceptableTokens() {
+        return new int[] { TokenTypes.METHOD_DEF };
+    }
+
+    @Override
+    public int[] getRequiredTokens() {
+        return getAcceptableTokens();
+    }
+
+    @Override
+    public int[] getDefaultTokens() {
+        return getAcceptableTokens();
+    }
+
+    @Override
+    public void visitToken(DetailAST ast) {
+        DetailAST slist = ast.findFirstToken(TokenTypes.SLIST);
+
+        if (slist != null && hasAnnotation(ast, "Test")) {
+            if (isEmptyMethodBody(slist)) {
+                log(ast.getLineNo(), "Empty Test detectado");
+            }
+        }
+    }
+
+    private boolean isEmptyMethodBody(DetailAST slist) {
+        DetailAST child = slist.getFirstChild();
+
+        // Percorre todos os nós filhos do SLIST
+        while (child != null) {
+            // Ignora ponto-e-vírgula vazios (ex: { ; })
+            if (child.getType() != TokenTypes.SEMI) {
+                return false;
+            }
+            child = child.getNextSibling();
+        }
+        return true;
+    }
+
+    // Métodu Auxiliar
+    private boolean hasAnnotation(DetailAST methodAst, String annotationName) {
+        DetailAST modifiers = methodAst.findFirstToken(TokenTypes.MODIFIERS);
+        if (modifiers != null) {
+            for (DetailAST child = modifiers.getFirstChild(); child != null; child = child.getNextSibling()) {
+                if (child.getType() == TokenTypes.ANNOTATION) {
+                    DetailAST annotationIdent = child.findFirstToken(TokenTypes.IDENT);
+                    if (annotationIdent != null && annotationIdent.getText().equals(annotationName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
