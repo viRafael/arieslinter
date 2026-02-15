70b80d144b6de4212d6c65594e36ad3991d0324c
rafael vieira <mouravieira44@gmail.com>
2026-02-15 11:00:27 -0300

    refactoring: new abstract class & new constanst & CTL updated

---
+++ src/main/java/br/ufba/arieslinter/checks/abstracts/AbstractTestSmellCheck.java
@@ -0,0 +1,51 @@
+package br.ufba.arieslinter.checks.abstracts;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import java.util.Set;
+
+public abstract class AbstractTestSmellCheck extends AbstractCheck {
+
+    protected boolean hasAnnotation(DetailAST ast, String annotationName) {
+        DetailAST modifiers = ast.findFirstToken(TokenTypes.MODIFIERS);
+        if (modifiers == null)
+            return false;
+
+        for (DetailAST child = modifiers.getFirstChild(); child != null; child = child.getNextSibling()) {
+            if (child.getType() == TokenTypes.ANNOTATION) {
+                DetailAST annotationIdent = child.findFirstToken(TokenTypes.IDENT);
+                if (annotationIdent != null &&
+                        annotationIdent.getText().equals(annotationName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    protected boolean hasAnyAnnotation(DetailAST ast, Set<String> annotations) {
+        return annotations.stream().anyMatch(ann -> hasAnnotation(ast, ann));
+    }
+
+    protected DetailAST findParentMethod(DetailAST ast) {
+        DetailAST parent = ast.getParent();
+
+        while (parent != null) {
+            if (parent.getType() == TokenTypes.METHOD_DEF) {
+                return parent;
+            }
+            parent = parent.getParent();
+        }
+
+        return null;
+    }
+
+    protected String getMethodName(DetailAST methodCall) {
+        DetailAST firstChild = methodCall.getFirstChild();
+        if (firstChild.getType() == TokenTypes.DOT) {
+            return firstChild.getLastChild().getText();
+        }
+        return firstChild.getText();
+    }
+}
