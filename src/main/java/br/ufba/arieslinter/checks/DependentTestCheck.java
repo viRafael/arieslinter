f2b49f9bb8fc47639d7bb6ae5073fc59e8220ddd
rafael vieira <mouravieira44@gmail.com>
2026-02-17 00:59:27 -0300

    update: dependent test class

--- src/main/java/br/ufba/arieslinter/checks/DependentTestCheck.java
+++ src/main/java/br/ufba/arieslinter/checks/DependentTestCheck.java
@@ -1,7 +1,8 @@
 package br.ufba.arieslinter.checks;
 
+import br.ufba.arieslinter.checks.abstracts.AbstractTestSmellCheck;
+import br.ufba.arieslinter.checks.constants.TestAnnotations;
 import com.puppycrawl.tools.checkstyle.StatelessCheck;
-import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
@@ -10,19 +11,19 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-    //TODO: TESTAR CLASSE DependentTestCheck
-// Funciona de um forma limitada devido as nuanceas da natureza do Dependente Test
-// Ele verifica dentro de uma classe de teste se um teste chama outro teste
-
 @StatelessCheck
-public class DependentTestCheck extends AbstractCheck {
+public class DependentTestCheck extends AbstractTestSmellCheck {
     private String currentClass;
+    
+    // Métodos de teste por classe: classe -> conjunto de nomes de métodos @Test
     private final Map<String, Set<String>> classTestMethods = new HashMap<>();
-    private final Map<String, Set<String>> testDependencies = new HashMap<>();
+    
+    // Dependências detectadas: método teste -> conjunto de testes que ele chama
+    private final Map<String, Map<String, Integer>> testDependencies = new HashMap<>();
 
     @Override
     public int[] getAcceptableTokens() {
-        return new int[] { TokenTypes.CLASS_DEF, TokenTypes.METHOD_DEF };
+        return new int[] {TokenTypes.CLASS_DEF};
     }
 
     @Override
@@ -44,82 +45,174 @@ public class DependentTestCheck extends AbstractCheck {
 
     @Override
     public void visitToken(DetailAST ast) {
-        if (ast.getType() == TokenTypes.CLASS_DEF) {
-            currentClass = ast.findFirstToken(TokenTypes.IDENT).getText();
-            classTestMethods.put(currentClass, new HashSet<>());
-        } else if (ast.getType() == TokenTypes.METHOD_DEF) {
-            processMethod(ast);
+        if (!isTestClass(ast)) {
+            return;
         }
+
+        currentClass = getClassName(ast);
+        classTestMethods.put(currentClass, new HashSet<>());
+
+        analyzeTestClass(ast);
+        reportDependencies();
     }
 
-    private void processMethod(DetailAST methodAst) {
-        if (hasAnnotation(methodAst, "Test")) {
-            String methodName = methodAst.findFirstToken(TokenTypes.IDENT).getText();
-            classTestMethods.get(currentClass).add(methodName);
-            analyzeMethodBody(methodAst, methodName);
+    /**
+     * Verifica se é uma classe de teste.
+     */
+    private boolean isTestClass(DetailAST classDefAst) {
+        String className = getClassName(classDefAst);
+        if (className != null && 
+            (className.endsWith("Test") || className.endsWith("Tests"))) {
+            return true;
         }
+        return hasTestMethods(classDefAst);
     }
 
-    private void analyzeMethodBody(DetailAST methodAst, String currentTestMethod) {
-        Set<String> dependencies = new HashSet<>();
-        DetailAST slist = methodAst.findFirstToken(TokenTypes.SLIST);
-        if (slist != null) {
-            findMethodCalls(slist, dependencies);
-        }
-        testDependencies.put(currentTestMethod, dependencies);
+    /**
+     * Obtém o nome da classe.
+     */
+    private String getClassName(DetailAST classDefAst) {
+        DetailAST identNode = classDefAst.findFirstToken(TokenTypes.IDENT);
+        return identNode != null ? identNode.getText() : null;
     }
 
-    private void findMethodCalls(DetailAST node, Set<String> dependencies) {
-        DetailAST child = node.getFirstChild();
+    /**
+     * Verifica se tem métodos @Test.
+     */
+    private boolean hasTestMethods(DetailAST classDefAst) {
+        DetailAST objBlock = classDefAst.findFirstToken(TokenTypes.OBJBLOCK);
+        if (objBlock == null) {
+            return false;
+        }
+
+        DetailAST child = objBlock.getFirstChild();
         while (child != null) {
-            if (child.getType() == TokenTypes.METHOD_CALL) {
-                String calledMethod = getMethodName(child);
-                if (classTestMethods.get(currentClass).contains(calledMethod)) {
-                    dependencies.add(calledMethod);
+            if (child.getType() == TokenTypes.METHOD_DEF) {
+                if (hasAnyAnnotation(child, TestAnnotations.ALL_TEST_ANNOTATIONS)) {
+                    return true;
                 }
             }
-            findMethodCalls(child, dependencies);
+            child = child.getNextSibling();
+        }
+        return false;
+    }
+
+    /**
+     * Analisa toda a classe de teste.
+     */
+    private void analyzeTestClass(DetailAST classDefAst) {
+        DetailAST objBlock = classDefAst.findFirstToken(TokenTypes.OBJBLOCK);
+        if (objBlock == null) {
+            return;
+        }
+
+        // Primeira passada: coleta nomes de todos os métodos @Test
+        DetailAST child = objBlock.getFirstChild();
+        while (child != null) {
+            if (child.getType() == TokenTypes.METHOD_DEF) {
+                if (hasAnyAnnotation(child, TestAnnotations.ALL_TEST_ANNOTATIONS)) {
+                    String methodName = getMethodName(child);
+                    classTestMethods.get(currentClass).add(methodName);
+                }
+            }
+            child = child.getNextSibling();
+        }
+
+        // Segunda passada: analisa dependências
+        child = objBlock.getFirstChild();
+        while (child != null) {
+            if (child.getType() == TokenTypes.METHOD_DEF) {
+                if (hasAnyAnnotation(child, TestAnnotations.ALL_TEST_ANNOTATIONS)) {
+                    analyzeTestMethod(child);
+                }
+            }
             child = child.getNextSibling();
         }
     }
 
-    private String getMethodName(DetailAST methodCall) {
-        DetailAST dot = methodCall.findFirstToken(TokenTypes.DOT);
-        if (dot != null) {
-            return dot.getLastChild().getText();
+    /**
+     * Analisa um método de teste em busca de chamadas a outros testes.
+     */
+    private void analyzeTestMethod(DetailAST testMethodAst) {
+        String testMethodName = getMethodName(testMethodAst);
+        Map<String, Integer> dependencies = new HashMap<>();
+
+        DetailAST methodBody = testMethodAst.findFirstToken(TokenTypes.SLIST);
+        if (methodBody != null) {
+            scanForTestCalls(methodBody, dependencies);
         }
-        return methodCall.findFirstToken(TokenTypes.IDENT).getText();
+
+        if (!dependencies.isEmpty()) {
+            testDependencies.put(testMethodName, dependencies);
+        }
     }
 
-    @Override
-    public void leaveToken(DetailAST ast) {
-        if (ast.getType() == TokenTypes.CLASS_DEF) {
-            reportDependencies();
-            currentClass = null;
+    /**
+     * Escaneia recursivamente por chamadas a outros métodos de teste.
+     */
+    private void scanForTestCalls(DetailAST node, Map<String, Integer> dependencies) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.getType() == TokenTypes.METHOD_CALL) {
+            String calledMethod = extractMethodName(node);
+            
+            // Verifica se é chamada a outro método de teste da mesma classe
+            if (calledMethod != null && 
+                classTestMethods.get(currentClass).contains(calledMethod)) {
+                dependencies.put(calledMethod, node.getLineNo());
+            }
+        }
+
+        // Continua busca recursiva
+        DetailAST child = node.getFirstChild();
+        while (child != null) {
+            scanForTestCalls(child, dependencies);
+            child = child.getNextSibling();
         }
     }
 
+    /**
+     * Extrai o nome do método de uma chamada METHOD_CALL.
+     */
+    private String extractMethodName(DetailAST methodCall) {
+        DetailAST firstChild = methodCall.getFirstChild();
+        if (firstChild == null) {
+            return null;
+        }
+
+        // Chamada qualificada: obj.method() ou this.method()
+        if (firstChild.getType() == TokenTypes.DOT) {
+            DetailAST methodNameNode = firstChild.getLastChild();
+            return methodNameNode != null ? methodNameNode.getText() : null;
+        }
+
+        // Chamada direta: method()
+        if (firstChild.getType() == TokenTypes.IDENT) {
+            return firstChild.getText();
+        }
+
+        return null;
+    }
+
+    /**
+     * Reporta todas as dependências detectadas.
+     */
     private void reportDependencies() {
-        for (Map.Entry<String, Set<String>> entry : testDependencies.entrySet()) {
-            for (String dependency : entry.getValue()) {
-                log(0, "Dependent Test detectado: '" + entry.getKey() + "' depende de '" + dependency + "'");
-            }
-        }
-    }
+        for (Map.Entry<String, Map<String, Integer>> entry : testDependencies.entrySet()) {
+            String testMethod = entry.getKey();
+            Map<String, Integer> dependencies = entry.getValue();
 
-    // Métodu auxiliar
-    private boolean hasAnnotation(DetailAST methodAst, String annotationName) {
-        DetailAST modifiers = methodAst.findFirstToken(TokenTypes.MODIFIERS);
-        if (modifiers != null) {
-            for (DetailAST child = modifiers.getFirstChild(); child != null; child = child.getNextSibling()) {
-                if (child.getType() == TokenTypes.ANNOTATION) {
-                    DetailAST annotationIdent = child.findFirstToken(TokenTypes.IDENT);
-                    if (annotationIdent != null && annotationIdent.getText().equals(annotationName)) {
-                        return true;
-                    }
-                }
+            for (Map.Entry<String, Integer> dep : dependencies.entrySet()) {
+                String dependentTest = dep.getKey();
+                int line = dep.getValue();
+
+                log(line,
+                    "Dependent Test: Test method ''{0}'' calls another test method ''{1}''. "
+                        + "Tests should be independent and not call each other.",
+                    testMethod, dependentTest);
             }
         }
-        return false;
     }
 }
